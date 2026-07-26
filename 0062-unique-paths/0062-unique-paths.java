121f87ac74ef880974d1719117fed07d8825cd26
Dev Aryan Jain <devaryan7704@gmail.com>
2026-07-26 23:36:08 +0530

    Time: 0 ms (100%), Space: 42.1 MB (48.38%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,27 @@
 class Solution {
+
+    public int uniquePaths(int m, int n)
+    {
+        int dp[][] = new int[m][n];
+        for(int i = 0 ; i < m ; i++)
+        {
+            dp[i][0] = 1;
+        }
+        for(int j = 0 ; j < n ; j++)
+        {
+            dp[0][j] = 1;
+        }
+
+        for(int i = 1; i < m ;i++)
+        {
+            for(int j = 1 ; j < n ; j++)
+            {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
     // public int uniquePaths(int m, int n)
     // {
     //     int dp[][] = new int[m][n];
@@ -19,22 +42,25 @@ class Solution {
     //     return dp[i][j];
     // }
 
-    public int uniquePaths(int m, int n)
-    {
-        int dp[][] = new int[m][n];
-        dp[m-1][n-1] = 1;
-        for(int i = m-1 ; i >= 0 ; i-- )
-        {
-            for(int j = n-1 ; j >= 0; j--)
-            {
-                if(i==m-1 && j==n-1) continue;
-                int down = 0; 
-                int right = 0;
-                if(i+1 < m) down = dp[i+1][j];
-                if(j+1 < n) right = dp[i][j+1];
-                dp[i][j] = down + right;
-            }
-        }
-        return dp[0][0];
-    }
+
+
+
+    // public int uniquePaths(int m, int n)
+    // {
+    //     int dp[][] = new int[m][n];
+    //     dp[m-1][n-1] = 1;
+    //     for(int i = m-1 ; i >= 0 ; i-- )
+    //     {
+    //         for(int j = n-1 ; j >= 0; j--)
+    //         {
+    //             if(i==m-1 && j==n-1) continue;
+    //             int down = 0; 
+    //             int right = 0;
+    //             if(i+1 < m) down = dp[i+1][j];
+    //             if(j+1 < n) right = dp[i][j+1];
+    //             dp[i][j] = down + right;
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
 }
